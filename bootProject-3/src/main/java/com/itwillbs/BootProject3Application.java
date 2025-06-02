f0be91a8b3a3a313d857ecd8106ec0e1f244a78f
hongcine <hongcine@naver.com>
2025-06-02 13:57:51 +0900

    cc

---
+++ bootProject-3/src/main/java/com/itwillbs/BootProject3Application.java
@@ -0,0 +1,13 @@
+package com.itwillbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootProject3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootProject3Application.class, args);
+	}
+
+}
